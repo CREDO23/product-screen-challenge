5c0b27a1dc62da58c9f8c191f921a702b0ab6c80
CREDO23 <thierrybakera12@gmail.com>
2022-12-04 10:49:06 +0200

    feat : add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Product from "./screens/Product";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default function () {
   return (
-    <div className="w-[100vw] h-[100vh] font-Poppins overflow-auto no-scrollbar">
+    <div className="w-[100vw] h-[100vh]  font-Poppins overflow-auto no-scrollbar">
+      <Navbar />
       <Product />
+      <Footer />
     </div>
   );
 }
--- src/screens/Product.jsx
+++ src/screens/Product.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar";
+
 import Description from "../components/Product-screen/Description";
 import Carousel from "../components/Product-screen/Carousel";
 import LeftSide from "../components/Product-screen/LeftSide";
@@ -6,15 +6,11 @@ import jord from '../assets/Images/jord1.png'
 
 export default function () {
   return (
-    <div className="w-full flex flex-col no-scrollbar items-center h-full">
-      <div className="w-full">
-        <Navbar />
-      </div>
-      <div className="w-full my-5 gap-10 flex-wrap px-3 justify-center flex h-full">
+      <div className="w-full my-5 gap-10 flex-wrap overflow-auto no-scrollbar px-3 justify-center flex h-full">
         <LeftSide />
         <Carousel />
         <Description  image={jord}/>
       </div>
-    </div>
+    
   );
 }
